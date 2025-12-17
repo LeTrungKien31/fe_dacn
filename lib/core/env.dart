51ac7ca411a3141ff67bf851e7cc2cf590c305e9
LeTrungKien31 <trungkien.12a2qtbt.2022@gmail.com>
2025-12-18 00:42:57 +0700

    Update env.dart

--- lib/core/env.dart
+++ lib/core/env.dart
@@ -2,8 +2,8 @@ class Env {
   // Chỉ chứa base URL, KHÔNG có /api/v1
   static const apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'https://be-dacn-production.up.railway.app',
-// http://10.0.2.2:8080
+    defaultValue: 'http://10.0.2.2:8080',
+    //defaultValue: 'https://be-dacn-production.up.railway.app',
     // ✅ Chỉ host:port
   );
 }
